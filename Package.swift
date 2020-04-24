64e9676c8a0f79e3de8aabe71a8730ac95471ae4
tiennguyen <tiennguyen@teqnological.asia>
2020-04-24 14:23:13 +0700

    revert rxswift dependency from 5.0.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RxTheme",
     platforms: [
-        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
+        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.1.0" ),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0" ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
